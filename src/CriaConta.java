62424af8cc21b2a28d576c91bb864d806cf45762
Gabriel <g.dessilva95@gmail.com>
2020-07-23 22:43:22 -0300

    Java Parte 2 - Aulas 1 e 2

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -1,6 +1,6 @@
 public class CriaConta {
     public static void main(String[] args) {
-        Conta primeiraConta =new Conta();
+        Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
         System.out.println(primeiraConta.saldo);
 
@@ -8,6 +8,27 @@ public class CriaConta {
         System.out.println(primeiraConta.saldo);
 
         Conta segundaConta = new Conta();
-        segundaConta.saldo = 50;
+        segundaConta.saldo = 300;
+
+        System.out.println("primeira conta tem " + primeiraConta.saldo);
+        System.out.println("segunda conta tem " + segundaConta.saldo);
+
+        segundaConta.numeroDaAgencia = 146;
+        System.out.println(primeiraConta.numeroDaAgencia);
+        System.out.println(primeiraConta.numeroDaContaCorrente);
+
+        System.out.println(segundaConta.numeroDaAgencia);
+
+        segundaConta.numeroDaAgencia = 146;
+        System.out.println("agora a segunda conta está na agencia " + segundaConta.numeroDaAgencia);
+
+        if (primeiraConta == segundaConta) {
+            System.out.println("mesma conta");
+        } else {
+            System.out.println("contas diferentes");
+        }
+
+        System.out.println(primeiraConta);
+        System.out.println(segundaConta);
     }
 }
